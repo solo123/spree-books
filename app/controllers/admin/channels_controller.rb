9a8d364e416816e0967ef5b59e911c0849802b47
Jimmy <jimmy@coolpur.com>
2011-05-04 17:50:52 +0800

    aa

--- app/controllers/admin/channels_controller.rb
+++ app/controllers/admin/channels_controller.rb
@@ -1,3 +1,16 @@
 class Admin::ChannelsController < Admin::BaseController
   resource_controller
+  
+  index.before do
+		@companies = Company.all
+		@current = params[:company] ? params[:company].to_i : 0
+  end
+  
+  def collection
+  	if params[:company] && params[:company] != '0'
+  		@object ||= end_of_association_chain.find_all_by_company_id(params[:company])
+  	else
+  		@object ||= end_of_association_chain.all
+  	end
+  end
 end
