109cd0016a2fb77d659f9092864b99f4f6fdf31f
Jimmy <jimmy@coolpur.com>
2011-05-04 16:26:53 +0800

    ss

--- lib/bkb_hooks.rb
+++ lib/bkb_hooks.rb
@@ -6,7 +6,7 @@ class BkbHooks < Spree::ThemeSupport::HookListener
   end
   
   insert_after :admin_configurations_menu do
-  	'<tr><td><a href="/admin/book_configuration">Reader配置</a></td>配置手机书城软件移动基地等参数<td>efg</td></tr>'
+  	'<tr><td><a href="/admin/book_configuration">Reader配置</a></td><td>配置手机书城软件移动基地等参数</td></tr>'
   end
   
   insert_before :account_summary do
