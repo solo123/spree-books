83a17cbf3d3f9162bb300e6fc174f3fe98a49fec
Jimmy <jimmy@coolpur.com>
2011-05-17 13:48:00 +0800

    charts

--- app/controllers/admin/charts_controller.rb
+++ app/controllers/admin/charts_controller.rb
@@ -1,5 +1,13 @@
 class Admin::ChartsController < Admin::BaseController
   def index
   	@balances = Balance.find_by_sql ["select  balance_date, sum(count) as cnt from balances group by balance_date"]
+  	s = []
+	 	@total_count=0
+	 	@balances.each do |b|
+	 		@total_count += b.cnt.to_i 
+	 		s << "[#{b.balance_date.to_i * 1000}, #{b.cnt.to_i}]"
+	  end
+	  @query_data = []
+	  @query_data << s.join(',')
   end
 end
