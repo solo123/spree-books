ceac385ad23cf0e303beca839c1fd4e9799e9ed6
Jimmy <jimmy@coolpur.com>
2011-06-27 10:40:30 +0800

    aa

--- lib/bkb_hooks.rb
+++ lib/bkb_hooks.rb
@@ -2,7 +2,7 @@
 class BkbHooks < Spree::ThemeSupport::HookListener
   # custom hooks go here
   insert_after :admin_tabs do
-  	%(<%= tab(:books) %> <%= tab(:charts)%> <%= tab(:cm_users) %>)
+  	%(<%= tab(:books) %> <%= tab(:charts)%> <%= tab(:cm_users) %> <%= tab(:waps))
   end
   
   insert_after :admin_configurations_menu do
